2601449fea1b200ebf7833b6a521f70901ad053e
pooh <cktjdgus45@naver.com>
2022-12-01 21:39:51 +0900

    practice styled component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from './Button.module.css';
+import styled, { css } from 'styled-components';
+
+const ButtonWrapper = styled.button`
+    width: 30%;
+    height: 30px;
+    background-color: aliceblue;
+    border-radius: 3px;
+    border: none;
+    ${(props) =>
+        props.primary &&
+        css`
+        background: #009cd5;
+        color:white;
+    `}
+`
 
 const Button = () => {
     return (
-        <button className={styles.button}>버튼</button>
+        <>
+            <ButtonWrapper >버튼</ButtonWrapper>
+            <ButtonWrapper primary>버튼</ButtonWrapper>
+        </>
     )
 }
 
