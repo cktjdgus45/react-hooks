3b21f7e17f12d1d76296cc8f70a98c4d55c6e0d6
pooh <cktjdgus45@naver.com>
2022-12-05 21:23:29 +0900

    css-border

--- src/components/todoApp/header/Header.jsx
+++ src/components/todoApp/header/Header.jsx
@@ -7,9 +7,9 @@ import { BsSunFill } from 'react-icons/bs'
 const Header = () => {
     return (
         <header className={styles.header}>
-            <BsSunFill className={styles.theme}/>
+            <BsSunFill className={styles.theme} />
             <div className={styles.navbar}>
-                <div className={styles.navItem}>All</div>
+                <div className={`${styles.navItem} ${styles.selected}`}>All</div>
                 <div className={styles.navItem}>Active</div>
                 <div className={styles.navItem}>Completed</div>
             </div>
