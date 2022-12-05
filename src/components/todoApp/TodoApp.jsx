2dca58f33857df10b7b2f02c317f4060ec5bb0ca
pooh <cktjdgus45@naver.com>
2022-12-05 16:32:44 +0900

    css-checkbox

--- src/components/todoApp/TodoApp.jsx
+++ src/components/todoApp/TodoApp.jsx
@@ -5,8 +5,6 @@ import TodoList from './main/TodoList';
 import styles from './TodoApp.module.css';
 
 const TodoApp = () => {
-    // /* #1A1C33 header #222439 main * ##F1842F header,footer,checkbox font color /
-
     return (
         <div className={styles.bg}>
             <section className={styles.app}>
