6fddc40c2482b0e818be428666b2b16b2a45fb9e
pooh <cktjdgus45@naver.com>
2022-12-21 22:11:08 +0900

    apply tailwind

--- src/components/youtubeApp/header/Header.jsx
+++ src/components/youtubeApp/header/Header.jsx
@@ -4,6 +4,7 @@ import Search from './Search';
 const Header = () => {
     return (
         <header>
+            <h2 className='text-2xl'>유튜브</h2>
             {/* logo */}
             <Search />
         </header>
