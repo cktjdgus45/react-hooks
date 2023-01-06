7ae57292f529f085e5e222a9b40efed375b8af9d
pooh <cktjdgus45@naver.com>
2023-01-06 12:08:58 +0900

    add moment-react

--- src/components/youtubeApp/main/videoComponents/VideoCard.jsx
+++ src/components/youtubeApp/main/videoComponents/VideoCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import Moment from 'react-moment';
 
 const VideoCard = ({ video }) => {
     let navigate = useNavigate();
@@ -12,13 +13,17 @@ const VideoCard = ({ video }) => {
             }
         });
     }
+
+    console.log(publishedAt.slice(0, 10))
     return (
         <div className='bg-slate-600 cursor-pointer' onClick={onVideoCardClick}>
             <img alt="thumbnail" src={url} className='bg-orange-300' />
             <div className='h-2/5 flex flex-col'>
                 <h1 className='text-white text-base mb-4'>{title}</h1>
                 <span className='text-slate-400 text-xs'>{channelTitle}</span>
-                <span className='text-slate-400 text-xs'>{publishedAt}</span>
+                <span className='text-slate-400 text-xs'>
+                    <Moment fromNow>{publishedAt}</Moment>
+                </span>
             </div>
         </div>
     )
