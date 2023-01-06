2792af8b70418801dec2678efa7abf4e6762d648
pooh <cktjdgus45@naver.com>
2023-01-06 22:15:17 +0900

    route to home when click logo

--- src/components/youtubeApp/header/Header.jsx
+++ src/components/youtubeApp/header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Search from './Search';
 import { BsYoutube } from 'react-icons/bs';
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    let navigate = useNavigate();
+    const onLogoClick = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
     return (
         <header className='flex w-full justify-between items-center p-1 h-16 gap-x-60'>
-            <div className='flex items-center'>
+            <div className='flex items-center cursor-pointer' onClick={onLogoClick}>
                 <BsYoutube className='text-red-700 text-2xl' />
                 <h2 className='text-xl text-white ml-1'>Youtube</h2>
             </div>
