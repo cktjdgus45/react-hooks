6609549b26acaf5eca5a880728ea6ab0944d0506
pooh <cktjdgus45@naver.com>
2023-02-02 13:55:00 +0900

    add content to Product Component

--- src/components/shoppy/main/home/Product.jsx
+++ src/components/shoppy/main/home/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({ product }) => {
         size,
         url, id } = product;
     return (
-        <div className='flex flex-col shadow-lg rounded-md'>
+        <div className='flex flex-col shadow-lg rounded-md hover:scale-105 cursor-pointer transition ease-in-out duration-300'>
             <img className='basis-11/12 rounded-md' src={url} alt={description} />
             <div className='basis-1/12 p-2 flex-col justify-center'>
                 <div className='flex justify-between text-sm'>
--- src/components/shoppy/main/products/Products.jsx
+++ src/components/shoppy/main/products/Products.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import DB from '../../service/firebase/database';
+import Product from '../home/Product';
 
 const Products = () => {
-    return(
-        <>
-            products 집합소.
-        </>
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        new DB().read(setProducts);
+    }, []);
+    return (
+        <main className='w-full h-full'>
+
+            <section className='px-2 grid grid-cols-4 grid-rows-[repeat(20,500px)] gap-2 h-full'>
+                {products.map(product => (
+                    <Product key={product.id} product={product}></Product>
+                ))}
+            </section>
+        </main>
     )
 }
 
